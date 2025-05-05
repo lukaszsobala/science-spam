132eba82466f97b66fc90fe0d24e89de5438a7d9
lukaszsobala <lsobala1@gmail.com>
2025-05-05 15:36:19 +0200

    AI comments and fixes

--- generate_assassin.sh
+++ generate_assassin.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-# Generates the spamassassin file in one go
+# ---------------------------------------------------------------
+# SpamAssassin Domain List Generator
+# 
+# This script converts a plain list of spam domains in spamdomains.txt
+# into a SpamAssassin-compatible blacklist format.
+# Output is saved to spamdomains-assassin.txt
+# ---------------------------------------------------------------
 
-sed -e 's/^/blacklist_from\ \*\@\*/g' -e 's/$/\*/g' -e '/\*.*\./s/\*$//' -e '/\.$/s/\./\.\*/' spamdomains.txt > spamdomains-assassin.txt
+INPUT_FILE="spamdomains.txt"
+OUTPUT_FILE="spamdomains-assassin.cf"
+
+# Process the domains with the following transformations:
+#  1. Add "blacklist_from *@*" prefix to each domain
+#  2. Add "*" suffix to each line
+#  3. Remove trailing "*" if line already contains "*" and a dot
+#  4. Replace trailing dot with ".*"
+
+sed -e 's/^/blacklist_from\ \*\@\*/g' \
+    -e 's/$/\*/g' \
+    -e '/\*.*\./s/\*$//' \
+    -e '/\.$/s/\./\.\*/' \
+    "${INPUT_FILE}" > "${OUTPUT_FILE}"
+
+echo "SpamAssassin blacklist generated: ${OUTPUT_FILE}"
 
 exit 0
